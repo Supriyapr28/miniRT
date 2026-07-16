a050cd211397ab555cf3d5fc69694cff3c6edcca
Ursilapradeep Vadakkumpuram <uvadakku@3-E-18.42heilbronn.de>
2026-07-16 14:31:00 +0200

    Triggered_Esc/close for window

--- includes/rt.h
+++ includes/rt.h
@@ -1,10 +1,38 @@
 #ifndef RT_H
 # define RT_H
 
-# include "render.h"
+# include "mlx.h"
 
 # define WIN_WIDTH 800
 # define WIN_HEIGHT 600
 # define BLUE_COLOR 0x0000FF
 
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct s_mlx
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_mlx;
+
+int	    handle_key(int keycode, t_mlx *mlx);
+int	    handle_close(t_mlx *mlx);
+void	setup_hooks(t_mlx *mlx);
+
+t_mlx	*init_mlx(void);
+void	destroy_mlx(t_mlx *mlx);
+void	put_pixel(t_mlx *mlx, int x, int y, int color);
+
+int     create_image(t_mlx *mlx);
+void    render_color(t_mlx *mlx, int color);
+t_mlx   *fill_color(t_mlx *mlx);
+t_mlx  *start_mlx(void);
+
 #endif
+
--- src/main.c
+++ src/main.c
@@ -1,65 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uvadakku <uvadakku@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/16 12:03:57 by uvadakku          #+#    #+#             */
+/*   Updated: 2026/07/16 14:18:11 by uvadakku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rt.h"
-#include <stdio.h>
 
-static t_mlx	*fill_color(t_mlx *mlx)
+int create_image(t_mlx *mlx)
 {
-	int	x;
-	int	y;
-
-	mlx->img = mlx_new_image(mlx->mlx, WIN_WIDTH, WIN_HEIGHT);
-	if (!mlx->img)
-	{
-		printf("failed to create an image\n");
-		destroy_mlx(mlx);
-		return (NULL);
-	}
-	mlx->addr = mlx_get_data_addr(
-		mlx->img,
-		&mlx->bpp,
-		&mlx->line_len,
-		&mlx->endian
-	);
-	y = 0;
-	while (y < WIN_HEIGHT)
-	{
-		x = 0;
-		while (x < WIN_WIDTH)
-		{
-			put_pixel(mlx, x, y, BLUE_COLOR);
-			x++;
-		}
-		y++;
-	}
-	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
-	return (mlx);
+    mlx->img = mlx_new_image(mlx->mlx, WIN_WIDTH, WIN_HEIGHT);
+    if (!mlx->img)
+    {
+        printf("failed to create an image\n");
+        destroy_mlx(mlx);
+        return (0);
+    }
+    mlx->addr = mlx_get_data_addr(
+        mlx->img,
+        &mlx->bpp,
+        &mlx->line_len,
+        &mlx->endian
+    );
+    return (1);
 }
 
-int	main(int argc, char **argv)
+void render_color(t_mlx *mlx, int color)
 {
-	t_mlx	*mlx;
+    int x;
+    int y;
 
-	if (argc > 2)
-	{
-		printf("usage: %s [scene.rt]\n", argv[0]);
-		return (1);
-	}
-	(void)argv;
-	mlx = init_mlx();
-	if (mlx == NULL)
-	{
-		printf("failed to initialize the connection with minilibx\n");
-		return (1);
-	}
-	mlx->win = mlx_new_window(mlx->mlx, WIN_WIDTH, WIN_HEIGHT, "miniRT");
-	if (mlx->win == NULL)
-	{
-		printf("failed to create the window\n");
-		destroy_mlx(mlx);
-		return (1);
-	}
-	if (fill_color(mlx) == NULL)
-		return (1);
-	mlx_loop(mlx->mlx);
-	destroy_mlx(mlx);
-	return (0);
+    y = 0;
+    while (y < WIN_HEIGHT)
+    {
+        x = 0;
+        while (x < WIN_WIDTH)
+        {
+            put_pixel(mlx, x, y, color);
+            x++;
+        }
+        y++;
+    }
+}
+
+t_mlx *fill_color(t_mlx *mlx)
+{
+    if (!create_image(mlx))
+        return (NULL);
+    render_color(mlx, BLUE_COLOR);
+    mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
+    return (mlx);
+}
+
+t_mlx  *start_mlx(void)
+{
+    t_mlx  *mlx;
+
+    mlx = init_mlx();
+    if (!mlx)
+    {
+        printf("failed to initialize the connection with minilibx\n");
+        return (NULL);
+    }
+    mlx->win = mlx_new_window(mlx->mlx, WIN_WIDTH, WIN_HEIGHT, "miniRT");
+    if (!mlx->win)
+    {
+        printf("failed to create the window\n");
+        destroy_mlx(mlx);
+        return (NULL);
+    }
+    if (fill_color(mlx) == NULL)
+        return (NULL);
+    return (mlx);
+}
+
+int main(int argc, char **argv)
+{
+    t_mlx *mlx;
+
+    if (argc > 2)
+    {
+        printf("usage: %s [scene.rt]\n", argv[0]);
+        return (1);
+    }
+    mlx = start_mlx();
+    if (!mlx)
+        return (1);
+    setup_hooks(mlx);
+    mlx_loop(mlx->mlx);
+    destroy_mlx(mlx);
+    return (0);
 }
